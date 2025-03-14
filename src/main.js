bed24d2e38cfc14974ed63eb241d718554effcd0
TaufikM3 <341.mth@gmail.com>
2025-03-14 13:04:59 +0700

    setting up registration page and installing tailwind css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import './assets/main.css'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +10,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
